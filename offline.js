e579dc36139973444e644a8c472a655ff00a2917
Necroman1977 <54731722+Necroman1977@users.noreply.github.com>
2019-12-24 18:32:17 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1577222149,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/fotos-sheet0.png",
+		"images/fotos-sheet1.png",
+		"images/borde-sheet0.png",
+		"images/fondo-sheet0.png",
+		"images/sombra-sheet0.png",
+		"images/orden-sheet0.png",
+		"images/botones-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
